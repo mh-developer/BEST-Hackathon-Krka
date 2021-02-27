b568729086330b03eca7cdf4e0592518c2e42ad2
Mitja <mh7860@student.uni-lj.si>
2021-02-27 07:53:17 +0100

    feat: prepare warehouse domain models

---
+++ Hackathon/Hackathon.Domain/WarehouseAggregate/IWarehouseRepository.cs
@@ -0,0 +1,9 @@
+﻿using Hackathon.Domain.Shared;
+using System;
+
+namespace Hackathon.Domain.WarehouseAggregate
+{
+    public interface IWarehouseRepository : IRepository<Warehouse, Guid>
+    {
+    }
+}
---
+++ Hackathon/Hackathon.Domain/WarehouseAggregate/Warehouse.cs
@@ -0,0 +1,23 @@
+﻿using Hackathon.Domain.CompanyAggregate;
+using Hackathon.Domain.Shared;
+using System;
+
+namespace Hackathon.Domain.WarehouseAggregate
+{
+    public class Warehouse : Entity<Guid>
+    {
+        public int? MinCode { get; set; }
+
+        public int? MaxCode { get; set; }
+
+        public Company Company { get; set; }
+
+        public Guid? CompanyId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
