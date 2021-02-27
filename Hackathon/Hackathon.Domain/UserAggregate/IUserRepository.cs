64845e38bf08c89c5c29b9f34635dab5c76046cb
Mitja <mh7860@student.uni-lj.si>
2021-02-27 07:52:55 +0100

    feat: prepare user domain models

---
+++ Hackathon/Hackathon.Domain/UserAggregate/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Hackathon.Domain.Shared;
+using System;
+
+namespace Hackathon.Domain.UserAggregate
+{
+    public interface IUserRepository : IRepository<User, Guid>
+    {
+    }
+}
---
+++ Hackathon/Hackathon.Domain/UserAggregate/User.cs
@@ -0,0 +1,27 @@
+﻿using Hackathon.Domain.CompanyAggregate;
+using Hackathon.Domain.Shared;
+using Hackathon.Domain.WarehouseAggregate;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Hackathon.Domain.UserAggregate
+{
+    public class User : IdentityUser<Guid>, IEntity<Guid>, IHasDeletionTime
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletionTime { get; set; }
+
+        public Company Company { get; set; }
+
+        public Guid? CompanyId { get; set; }
+
+        public Warehouse Warehouse { get; set; }
+
+        public Guid? WarehouseId { get; set; }
+    }
+}
