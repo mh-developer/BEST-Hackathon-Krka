7f1ab996512eaf1e92aaa88572cd8128bbcdd4aa
Mitja <mh7860@student.uni-lj.si>
2021-02-27 08:22:23 +0100

    feat: add basic controllers and views

---
+++ Hackathon/Hackathon.MVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Hackathon.MVC.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Hackathon.MVC.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return RedirectToAction("Index", "Deliveries");
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            return RedirectToAction("Index", "Deliveries");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
